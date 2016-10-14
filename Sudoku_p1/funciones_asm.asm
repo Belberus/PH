77f8b98fb8d61a8107de1cd101d9dd1ba9140ba5
Alberto Martinez Menendez <amartinez95zgz@gmail.com>
2016-10-14 11:14:03 +0200

    Actualizado again

--- Sudoku_p1/funciones_asm.asm
+++ Sudoku_p1/funciones_asm.asm
@@ -66,100 +66,105 @@ for2:
 	CMP r7,#9		// Comprobamos si hemos recorrido todas las filas
 	BNE for2		// Si no hemos recorrido todas las filas volvemos al bucle
 
+	MOV r0,r6		// Colocamos en r0 el numero de celdas vacias para devolverlo al codigo invocador		
+
 sudoku_candidatos_propagar_arm:
 .arm
 	PUSH {lr}
-	MOV r4,#0	// Contador de pablos
+	MOV r4,#0				// Contador de posiciones recorridas
 	MOV r5, r1, LSL #5
 	ADD r6, r5, r2, LSL #1
 	ADD r6,r6,r0
-	LDRH r6, [r6]	// Cargamos el "valor"
+	LDRH r6, [r6]			// Cargamos el "valor"
 	
-	AND r6,r6, #0x000F	// Valor
+	AND r6,r6, #0x000F		// Valor
 	MOV r7,#0x0008
-	MOV r7,r7, LSL r5	// Mascara del valor
-	MVN r7,r7	// Invertimos el valor
+	MOV r7,r7, LSL r5		// Mascara del valor
+	MVN r7,r7				// Invertimos el valor
+	AND r7,r7, 0x00001111	// Valor final de la mascara
 
-	MOV r5,r0
+	MOV r5,r0				// Asignamos a r5 la primera posicion del tablero (hay que preparar la fila)
+
 // PROPAGAMOS POR LA FILA
 forFila:
-	LDRH r8,[r5]
+	LDRH r8,[r5]	// Cargamos el dato a mirar
 	AND r8,r8,r7	// Aplicamos la mascara
-	STRH r8,[r5]
+	STRH r8,[r5]	// Guardamos la mascara actualizada
 
-	ADD r4,r4,#1
-	ADD r5,r5,#2
-	CMP r4,#9
-	BNE forFila
+	ADD r4,r4,#1	// Suamos 1 al contador de filas
+	ADD r5,r5,#2	// Avanzamos a la siguiente posicion que mirar
+	CMP r4,#8		// Comparamos para saber si hemos llegado al final de la fila
+	BNE forFila		// Si todavia no hemos acabado en esa fila volvemos al bucle
 
 // PREPARAMOS LOS VALORES
 	MOV r5,#0
-	ADD r5,r0,r2	// Nos situamos en la primera posicion de la columna 
+	ADD r5,r0,r2	// Nos situamos en la primera posicion de la columna (r0 + columna)
+	MOV r4,#0		// Reseamos el contador 
 
 // PROPAGAMOS POR COLUMNA
 forColum:
-	LDRH r8,[r5]
+	LDRH r8,[r5]	// Cargamos el dato a mirar
 	AND r8,r8,r7	// Aplicamos la mascara
-	STRH r8,[r5]
+	STRH r8,[r5]	// Guardamos la mascara actualizada
 
-	ADD r4,r4,#1
-	ADD r5,r5,#32
-	CMP r4,#9
-	BNE forColum
+	ADD r4,r4,#1	// Incrementamos el numero de posiciones de la columna recorridas	
+	ADD r5,r5,#32	// Saltamos a la siguiente posicion de la columna
+	CMP r4,#8		// Comparamos para saber si hemos llegado a la ultima posicion de la columna
+	BNE forColum	// Si todavia no hemos acabado en esa columna volvemos al bucle
 
 // PREPARAMOS LOS DATOS
-	MOV r10,#0	// Resultado division fila
-	MOV r11,#0	// Resultado division columna
+	MOV r10,#0		// Resultado division fila
+	MOV r11,#0		// Resultado division columna
 	PUSH {r0,r1,r2}	// Guardamos los parametros para no modificarlos
+	MOV r4,#0		// Resultado de la division de la fila
+	MOV r9,#0		// Resultado de la division de la columna
 
+// Division de las filas
 divFila:
-	CMP r1,#3
-	SUBGE r1,r1,#3
-	ADDGE r4,r4,#1 
-	BGE divFila
+	CMP r1,#3		// Comparamos las filas con 3
+	SUBGE r1,r1,#3	// Si el numero de filas es mayor o igual que 3, restamos 3
+	ADDGE r4,r4,#1 	// Si el numero de filas es mayor o igual que 3, sumamos 1 al contador de division
+	BGE divFila		// Si el numero de filas es mayor o igual que 3, volvemos al bucle
 
+// Division de las columnas	
 divCol:
-	CMP r2,#3
-	SUBGE r2,r2,#3
-	ADDGE r9,r9,#1
-	BGE divCol
+	CMP r2,#3		// Comparamos las columnas con 3
+	SUBGE r2,r2,#3	// Si el numero de columnas es mayor o igual que 3, restamos 3
+	ADDGE r9,r9,#1	// Si el numero de columnas es mayor o igual que 3, sumamos 1 al contador de division
+	BGE divCol		// Si el numero de columnas es mayor o igual que 3, volvemos al bucle
 
-	MOV r1, #3
-	MUL r4,r10, r1	// Posicion inicial fila
-	MUL r9,r11, r1	// Posicion inicial columna
+// Preparacion de valores
+
+	MOV r1, #3	
+	MUL r3,r4, r1	// Asignamos a r3 la posicion incial de la fila (resultado * 3)
+	MUL r4,r9, r1	// Asignamos a r4 la posicion inicial de la columna (resultado *3)
 	
 	// Nos situamos en la posicion inicial del Sector
-	ADD r3, r0, r4, LSL #5
-	ADD r3,r3, r9, LSL #1
+	MOV r5,#0 
+	ADD r5, r0, r3, LSL #5	// Nos situamos en la fila correcta (r0 + (nFilas * 32))
+	ADD r5,r5, r5, LSL #1	// Nos situamos en la columna correcta (r5 + (nColumas * 2))
 
-	POP {r0,r1,r2}	// Recuperamos el valor de los parametrosç
+	POP {r0,r1,r2}			// Recuperamos el valor de los parametrosç
 
-	MOV r1, #0	// Contador filas
-	MOV r2, #0	// Contador columnas
-/*	
-	r0 cuadricula
-	r1 contador
-	r2 contador
-	r3 posicion inicial del cuadrante 
-	r7 mascara pablera ya preparado
-*/
+	MOV r4, #0	// Contador filas
+	MOV r6, #0	// Contador columnas
 
 // PROPAGAMOS POR LA REGION
 forRegion:
-	LDRH r8,[r3]
-	AND r8,r8,r7	// Aplicamos la mascara
-	STRH r8,[r3]
+	LDRH r8,[r5]		// Cargamos el dato a mirar
+	AND r8,r8,r7		// Aplicamos la mascara
+	STRH r8,[r3]		// Guardamos la mascara actualizada
 
-	ADD r1,r1,#1	// Incrementamos contador de fila	
-	CMP r1,#3
-	ADDGE r3,r3, #26	// Saltamos a la fila siguiente
-	MOVGE r1,#0
-	ADDGE r2,r2,#1
-	ADDLT r3,r3,#2	// Si no toca saltar fila avanzamos a la siguiente posicion
+	ADD r6,r6,#1		// Incrementamos en 1 el contador de columnas	
+	CMP r6,#3			// Comparamos las columnas recorridas con 3
+	ADDGE r3,r3, #26	// Si el numero de columnas recorridas es igual o mayor que 3, saltamos a la siguiente fila
+	ADDGE r4,r4,#1		// Si saltamos de fila sumamos 1 al contador de filas recorridas
+	MOVGE r6,#0			// Si el numero de columnas recorridas es igual o mayor que 3, reseteamos el contador de columnas
+	ADDLT r5,r5,#2		// Si el numero de columnas recorridas es menor que 3 avanzamos a la siguiente columna
 
-	CMP r2,#3
-	BLT forRegion
+	CMP r4,#3			// Comparamos el numero de filas recorridas con 3
+	BLT forRegion		// Si el numero de filas recorridas es menor que 3 volvemos al bucle
 
-	POP {pc}	// Volvemos a la rutina init
+	POP {pc}			// Volvemos a la rutina init
 
 
